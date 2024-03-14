5d6b54f0fca6785a9b110116fe547d2924e59c19
alexblad <alexander.m.blad@gmail.com>
2024-03-14 01:32:11 -0600

    Changed some things

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop02 World!");
+        
         //Test the RandomPromptGenerator
         //Test.TestOutput();
         
