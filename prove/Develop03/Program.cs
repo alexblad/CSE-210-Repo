18c612155c437ccbf1012a0357d1dd495fd1858e
alexblad <alexander.m.blad@gmail.com>
2024-03-27 02:57:44 -0600

    Commented out some things I didn't need.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,22 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Word w1 = new Word("bigboy");
-        w1.GetDisplayText();
-        Console.WriteLine(w1.GetDisplayText());
+    //This was all stuff that I used to test that my methods worked originially 
+        //Word w1 = new Word("bigboy");
+        //w1.GetDisplayText();
+        //Console.WriteLine(w1.GetDisplayText());
 
-        w1.Hide();
-        Console.WriteLine(w1.GetDisplayText());
+        //w1.Hide();
+        //Console.WriteLine(w1.GetDisplayText());
 
-        w1.Show();
-        Console.WriteLine(w1.GetDisplayText());
+        //w1.Show();
+        //Console.WriteLine(w1.GetDisplayText());
 
+        //Reference v1 = new Reference("Nephi", 1, 5);
+        //Reference v2 = new Reference("James", 1, 6, 9);
 
-        Reference v1 = new Reference("Nephi", 1, 5);
-        Reference v2 = new Reference("James", 1, 6, 9);
-
-        Console.WriteLine(v1.GetDisplayText());
-        Console.WriteLine(v2.GetDisplayText());
+        //Console.WriteLine(v1.GetDisplayText());
+        //Console.WriteLine(v2.GetDisplayText());
 
 //Get initial coniditions for the program
         string newverse = "yes";
