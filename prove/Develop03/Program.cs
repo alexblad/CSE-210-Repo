d61820fba2593b99fc7832ddc29a7dbe87bf2ced
alexblad <alexander.m.blad@gmail.com>
2024-03-27 01:50:58 -0600

    Worked on Scripture class.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -20,5 +20,43 @@ class Program
 
         Console.WriteLine(v1.GetDisplayText());
         Console.WriteLine(v2.GetDisplayText());
+
+//Get initial coniditions for the program
+        string newverse = "yes";
+
+        while (newverse.ToLower() == "yes")
+        {
+            Console.WriteLine("What book is your scripture in?");
+            string book= Console.ReadLine();
+            Console.WriteLine("What chapter?");
+            int chapt = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is the starting verse?");
+            int verse1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is the end verse?");
+            int verse2 = int.Parse(Console.ReadLine());
+            Reference newRef;
+            if (verse1 == verse2)
+            {
+                newRef = new Reference(book, chapt, verse1);
+            }
+            else
+            {
+                newRef = new Reference(book, chapt, verse1, verse2);
+            }
+            Console.WriteLine("What does the scripture say?");
+            string words = Console.ReadLine();
+            Scripture toMemorize = new Scripture(newRef, words);
+
+            Console.WriteLine(toMemorize.GetDisplayText());
+
+            Random random = new Random();
+            int randomToHide = random.Next(1,3);
+            toMemorize.HideWords(randomToHide);
+
+            Console.WriteLine(toMemorize.GetDisplayText());
+        }
+        Console.WriteLine("Would you like to try a new verse?");
+        newverse = Console.ReadLine();
+
     }
 }
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,72 @@
+using System.Text;
+
+public class Scripture 
+{
+//Attributes
+    private Reference _reference; 
+    private List<Word> _words;
+
+//Methods
+    //Constructors
+    public Scripture(Reference reference, string words)
+    {
+        _reference = reference;
+        _words = CreateWordList(words);
+
+
+    }
+
+    //Other methods
+    private List<Word> CreateWordList(string words)
+    {
+        List<Word> scriptureWords = new List<Word>();
+        string[] wordArray = words.Split(new char[] {' ', ',', ':', ';', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+        foreach (string wordText in wordArray)
+        {
+            Word word = new Word(wordText);
+            scriptureWords.Add(word);
+        }
+        return scriptureWords;
+    }
+
+    public void HideWords(int numberToHide)
+    {
+        //Check if numberToHide is valid
+        if (numberToHide <= 0 || numberToHide > _words.Count)
+        {
+            throw new ArgumentException("Invalid number of words to hide.");
+        }
+
+        //Create a list of indeces for the words in the verse
+        List<int> indicesToHide = new List<int>();
+
+        Random random = new Random();
+        while (indicesToHide.Count < numberToHide)
+        {
+            int indexOfHidden = random.Next(_words.Count);
+            if (!indicesToHide.Contains(indexOfHidden))
+            {
+                indicesToHide.Add(indexOfHidden);
+            }
+        }
+
+        foreach(int index in indicesToHide)
+        {
+            _words[index].Hide();
+        }
+    }
+    //Getters
+    public string GetDisplayText()
+    {
+        StringBuilder sb = new StringBuilder();
+        foreach (var word in _words)
+        {
+            sb.Append(word.GetDisplayText());
+            sb.Append(" "); //Add space between words
+
+        }
+        return ($"{_reference.GetDisplayText()} {sb.ToString()}");
+
+    }
+    
+}
